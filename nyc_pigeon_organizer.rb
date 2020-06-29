d085e23163e6f738a573bc93c319151475c0e117
Micah Mosley <micah.mosley@utexas.edu>
2020-06-29 02:19:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  
+  names=[]
+  names=data[:color].map do |color, pigeon|
+    pigeon
+  end 
+  names=names.flatten
+  names=names.uniq!
+  new_data={}
+  i=0 
+  while i<names.length do 
+    new_data[names[i]]={color: [],
+      gender: [],
+      lives: []
+    }
+    i+=1 
+  end 
+  
+  data[:color].each do |col, pigeon|
+    new_data[pigeon][:color].push(col.to_s)
+  end
+  
+  data[:gender].each do |gend, pigeon|
+    new_data[pigeon][:gender].push(gend.to_s)
+  end
+  
+  data[:lives].each do |liv, pigeon|
+    new_data[pigeon][:lives].push(liv.to_s)
+  end
+  binding.pry
+  new_data
+  
+  
+  
+  
+  
+  
 end
